d92d8b4155336bcb90dfe7cf9f2487a05099b89b
sanamerchant <123655714+sanamerchant@users.noreply.github.com>
2024-04-24 20:06:57 -0400

    saving data based on keyboard

--- code/data/data_retrieval.py
+++ code/data/data_retrieval.py
@@ -1,3 +1,4 @@
+import os
 from PIL import ImageGrab
 import cv2
 import numpy as np
@@ -5,31 +6,37 @@ import keyboard
 
 all_frames = np.array([])
 
+left_count = 1
+right_count = 1
+none_count = 1
+
 while(True):
     #getting frame
     img = ImageGrab.grab(bbox=(100, 10, 600, 300)) #x, y, width, height
     img_np = np.array(img) #convert image to numpy array
     frame = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY) #convert to grayscale
-    all_frames = np.append(all_frames, frame) #append to all_frames
-    
-    cv2.imshow("frame", frame) #display frame
 
-    keyboard_values = np.array([])
+    directory = r"C:/Users/smerc/OneDrive/Desktop/Sophomore Year/CS1430_Projects/CV-Final-Project/code/data/train"
+    os.chdir(directory)
 
-    #TODO: ensure there's a keyboard value for each frame
     #recording keyboard info
-    event = keyboard.read_event()
+   
+    event = keyboard.read_event() 
     if event.event_type == "down":
         if event.name == "left":
-            keyboard_values = np.append(keyboard_values, "left")
-            print(keyboard_values)
+            os.chdir(directory+"/left")
+            cv2.imwrite("left" + str(left_count) + ".png", frame)
+            left_count += 1
         elif event.name == "right":
-            keyboard_values = np.append(keyboard_values, "right")
-            print(keyboard_values)
+            os.chdir(directory+"/right")
+            cv2.imwrite("right" + str(right_count) + ".png", frame)
+            right_count += 1
         elif event.name == "esc":
             break
-        else:
-            keyboard_values = np.append(keyboard_values, "none")
+    else:
+        os.chdir(directory+"/none")
+        cv2.imwrite("none" + str(none_count) + ".png", frame)
+        none_count += 1
 
     #break loop on escape
     key = cv2.waitKey(1) 
@@ -39,4 +46,4 @@ while(True):
 cv2.destroyAllWindows()
 print(all_frames)
 print(all_frames.shape)
-print(keyboard_values.shape)
+# print(keyboard_values.shape)

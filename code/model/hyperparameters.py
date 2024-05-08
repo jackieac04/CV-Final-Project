4a603828ead5ba4c7733af5293a9af36db2027af
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-05-08 19:46:44 -0400

    Feat: increased batch size

--- code/model/hyperparameters.py
+++ code/model/hyperparameters.py
@@ -41,7 +41,7 @@ max_num_weights = 5
 Defines the number of training examples per batch.
 You don't need to modify this.
 """
-batch_size = 10
+batch_size = 20
 
 """
 The number of image scene classes. Don't change this.

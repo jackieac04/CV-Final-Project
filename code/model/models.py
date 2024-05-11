d847211089bc5eaf14b30cb9ab4a69a6b092d8d6
prlakshm <pranavi_lakshminarayanan@brown.edu>
2024-05-11 11:26:09 -0400

    Fear: reset to original

--- code/model/models.py
+++ code/model/models.py
@@ -22,32 +22,43 @@ class VGGModel(tf.keras.Model):
         # Don't change the below:
 
         self.vgg16 = [
-              # Block 1
-              Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv1"),
-              Conv2D(64, 3, 1, padding="same", activation="relu", name="block1_conv2"),
-              MaxPool2D(2, name="block1_pool"),
-              # Block 2
-              Conv2D(64, 3, 1, padding="same", activation="relu", name="block2_conv1"),
-              Conv2D(64, 3, 1, padding="same", activation="relu", name="block2_conv2"),
-              MaxPool2D(2, name="block2_pool"),
-              # Block 3
-              Conv2D(128, 3, 1, padding="same", activation="relu", name="block3_conv1"),
-              Conv2D(128, 3, 1, padding="same", activation="relu", name="block3_conv2"),
-              Conv2D(128, 3, 1, padding="same", activation="relu", name="block3_conv3"),
-              MaxPool2D(2, name="block3_pool"),
-              # Block 4
-              Conv2D(256, 3, 1, padding="same", activation="relu", name="block4_conv1"),
-              Conv2D(256, 3, 1, padding="same", activation="relu", name="block4_conv2"),
-              Conv2D(256, 3, 1, padding="same", activation="relu", name="block4_conv3"),
-              MaxPool2D(2, name="block4_pool"),
-              # # Block 5
-              # Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv1"),
-              # Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv2"),
-              # Conv2D(512, 3, 1, padding="same", activation="relu", name="block5_conv3"),
-              # MaxPool2D(2, name="block5_pool"),
-       ]
-
-
+            # Block 1
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv1"),
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv2"),
+            MaxPool2D(2, name="block1_pool"),
+            # Block 2
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv1"),
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv2"),
+            MaxPool2D(2, name="block2_pool"),
+            # Block 3
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv1"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv2"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv3"),
+            MaxPool2D(2, name="block3_pool"),
+            # Block 4
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv3"),
+            MaxPool2D(2, name="block4_pool"),
+            # Block 5
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv3"),
+            MaxPool2D(2, name="block5_pool")
+        ]
 
     
         for layer in self.vgg16:
